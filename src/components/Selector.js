d903b5ccd8f8f6abb7a39f2b291d4a2c853ca2ac
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2022-12-14 12:25:00 -0500

    Progress in rules screen

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,39 +1,46 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import '../stylesheets/Selector.css'
 
-function Selector({type}){
+
+function Selector({type, columnsNumber}){
 
     const options = [];
 
     switch(type){
         case('logical'):
-            options.push(<button className="button button-logical">AND</button>);
-            options.push(<button className="button button-logical">OR</button>);
+            options.push(<Grid xs={1}><Button variant="contained"  className="button button-logical">AND</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained"  className="button button-logical">OR</Button></Grid>);
             break;
         case('comparation'):
-            options.push(<button className="button button-comparation">=</button>);
-            options.push(<button className="button button-comparation">'{'>'}'</button>);
-            options.push(<button className="button button-comparation">'{'<'}'</button>);
-            options.push(<button className="button button-comparation">'{">"}='</button>);
-            options.push(<button className="button button-comparation">'{"<"}='</button>);
+            options.push(<Grid xs={1}><Button variant="contained" onClick={()=>console.log("test")}className="button button-comparation">=</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-comparation">{'>'}</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-comparation">{'<'}</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-comparation">{">"}=</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-comparation">{"<"}=</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-comparation">{"!="}</Button></Grid>);
             break;            
         case('columns'):
-            options.push(<button className="button button-column">Column1</button>);
-            options.push(<button className="button button-column">Column2</button>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column1</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column2</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column1</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column2</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column1</Button></Grid>);
+            options.push(<Grid xs={1}><Button variant="contained" className="button button-column">Column2</Button></Grid>);
+            
             break;
         default:
     }
 
     return(
-        <div className={`selector ${type}`}>
-            
+        <div className={`selector ${type}`}>            
             <div className="selector-name">
                 {type}
             </div>
-            <div className="selector-options">
+            <Grid container spacing={2} columnSpacing={-1} columns={columnsNumber}>                
                 {options}
-            </div>
-
-            
+            </Grid>            
         </div>
     );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,19 @@
 import { render } from '@testing-library/react';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 //import {Suppliers} from "./components/Suppliers";
 import Selector from '../components/Selector.js'
-import '../stylesheets/Home.css'
+import '../stylesheets/Home.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+//import { DataGrid } from '@mui/x-data-grid';
 
 class Home extends Component{
+  
   render(){
+
     return (
       <div className='home'>
         
@@ -13,18 +21,40 @@ class Home extends Component{
           <h1>MOTOR DE REGLAS </h1>
         </div>
         <div className='expression-builder-selectors'>
-          <Selector type='logical'></Selector>
-          <Selector type='comparation'></Selector>
-          <Selector type='columns'></Selector>
+          <Selector type='logical' columnsNumber={1}></Selector>
+          <Selector type='comparation' columnsNumber={3}></Selector>
+          <Selector type='columns' columnsNumber={3}></Selector>
+        </div>        
+        <div className='rule-container'>
+          <Stack direction='row' spacing={2}>
+            <Box sx={{width: 500, maxWidth: '100%',}}>
+              <TextField fullWidth label="Regla" id="rule" />
+            </Box>
+            <Button variant="contained" endIcon={<SendIcon />}>Validar</Button>
+          </Stack>          
         </div>
+        <div>
+          <Button variant="contained" endIcon={<SendIcon />}>probar regla</Button>
+        </div>
+        <div>
 
+        </div>
 
       </div>
 
     )
   }
 }
-
+/*          <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+              disableSelectionOnClick
+              experimentalFeatures={{ newEditingApi: true }}
+              />
+          </Box>*/
 /*
 combobox()
 {
