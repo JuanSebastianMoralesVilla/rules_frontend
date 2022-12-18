fc3b7c5ccd49c073a2529a498dba523369ddd0ad
zonitidus <zonitidus@gmail.com>
2022-12-18 16:04:39 -0500

    Fix(Index): removed unused imports

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,6 +1,4 @@
 import React from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 import * as MdIcons from "react-icons/md";
 import * as BiIcons from "react-icons/bi";
