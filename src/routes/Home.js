4f3500eef90c235a346a2c21d257bdb10c86e8fc
Sebastian Morales <1193029087@u.icesi.edu.co>
2022-12-19 16:18:36 -0500

    Revert "Merge branch 'feature-back-integration' into develop"
    This reverts commit b454e5e48824a78064b224d12f01f9a1022f65c4, reversing
    changes made to 8f30c4c745edb5b7a18cdb19857ade4b8da264da.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,52 +15,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
 
 class Home extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      columns: [],
-      rows : []
-    };
-  }
-  
-  async getColumns (){    
-    const res = await fetch('http://localhost:8080/api/transactions/columns')
-    var columns = await res.json();
-
-    var columnsProcessed = [];
-    
-    for(var i = 0 ; i < columns.length; i++){
-      columnsProcessed.push( { field: columns[i] , headerName: columns[i]});
-    }
-
-    return columnsProcessed;
-  }
-
-  async getRows(){
-    const res = await fetch('http://localhost:8080/api/transactions/')
-    var rows = await res.json();
-
-    var rowsProcessed = [];
-      
-    for(var i = 0 ; i < rows.length; i++){
-      rowsProcessed.push(rows[i].data);
-    }
-
-    return rowsProcessed;
-  }
-
-  //async validateAndApplyRule(){
-
-  //}
-
-  render() {  
-
-    const fillTable = async () => {  
-      this.setState({columns: await this.getColumns()});
-      this.setState({rows: await this.getRows()});
-    };
-    
+  render() {
     const updateRule = (symbol) => {
       var input = document.getElementById("rule");
 
@@ -77,6 +32,39 @@ class Home extends Component {
       input.setSelectionRange(input.value.length, input.value.length);
       input.focus();
     };
+
+    const columns = [
+      { field: "id", headerName: "ID", width: 70 },
+      { field: "firstName", headerName: "First name", width: 130 },
+      { field: "lastName", headerName: "Last name", width: 130 },
+      {
+        field: "age",
+        headerName: "Age",
+        type: "number",
+        width: 90,
+      },
+      {
+        field: "fullName",
+        headerName: "Full name",
+        description: "This column has a value getter and is not sortable.",
+        sortable: false,
+        width: 160,
+        valueGetter: (params) =>
+          `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+      },
+    ];
+
+    const rows = [
+      { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+      { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+      { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+      { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+      { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+      { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+      { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+      { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+      { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    ];
 
     return (
       <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
@@ -99,10 +87,6 @@ class Home extends Component {
               <li>(Column 1 {">"} column 2) AND (Column 1 = 2)</li>
             </div>
           </p>
-
-          <Button variant="contained" onClick={fillTable} endIcon={<SendIcon />}>
-                          Fill table
-                        </Button>
 
           <table className="selectors-table">
             <thead>
@@ -180,12 +164,11 @@ class Home extends Component {
           </div>
 
           <div style={{ height: 400, width: "100%" }}>
-            <DataGrid id='main-table'
-              rows={this.state.rows}
-              columns={this.state.columns}
+            <DataGrid
+              rows={rows}
+              columns={columns}
               pageSize={5}
               rowsPerPageOptions={[5]}
-              getRowId={(row) => row.transaction_id}
             />
           </div>
         </main>
